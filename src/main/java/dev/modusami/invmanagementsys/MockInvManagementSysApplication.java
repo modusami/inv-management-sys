23d3ff22c321d49fc416a82cd573d6701b949088
modusami <128193218+modusami@users.noreply.github.com>
2024-03-16 22:40:14 -0400

    Initial Files

---
+++ src/main/java/dev/modusami/invmanagementsys/MockInvManagementSysApplication.java
@@ -0,0 +1,13 @@
+package dev.modusami.invmanagementsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockInvManagementSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockInvManagementSysApplication.class, args);
+	}
+
+}
